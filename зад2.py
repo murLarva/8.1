514c20e1995e41f429c5be84861e43b2ca67d2a2
murLarva <158815368+murLarva@users.noreply.github.com>
2024-05-27 10:08:27 +0300

    Add files via upload

---
+++ зад2.py
@@ -0,0 +1,26 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        friends_string = ''
+        for friend in DATABASE.keys():
+            friends_string += friend + ' '
+        return 'Твои друзья: ' + friends_string
+    elif query == 'Где все мои друзья?':
+        return 'Я поняла, это вопрос про города!'
+    else:
+        return '<неизвестный запрос>'
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
+print(process_anfisa('Где все мои друзья?'))
